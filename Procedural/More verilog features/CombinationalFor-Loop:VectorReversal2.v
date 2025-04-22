0708700a7ded1cc3d7390c82f897e2b77f43bff9
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-04-22 15:58:08 +0530

    Update CombinationalFor-Loop:VectorReversal2.v

--- Procedural/More verilog features/CombinationalFor-Loop:VectorReversal2.v
+++ Procedural/More verilog features/CombinationalFor-Loop:VectorReversal2.v
@@ -8,3 +8,30 @@ module top_module(
             out[i]=in[$bits(out)-i-1];
     end
 endmodule
+
+
+//method2
+module top_module( 
+    input [99:0] in,
+    output [99:0] out
+);
+    integer i;
+    always@(*)begin
+        for(i=0;i<100;i=i+1)
+            out[i]=in[100-i-1];
+    end
+endmodule
+
+
+
+//method3
+module top_module( 
+    input [99:0] in,
+    output [99:0] out
+);
+    integer i;
+    always@(*)begin
+        for(i=0;i<100;i=i+1)
+            out[99-i]=in[i];
+    end
+endmodule
