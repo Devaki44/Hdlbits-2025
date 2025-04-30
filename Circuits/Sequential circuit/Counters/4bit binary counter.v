d939c41a12ae1f4109711c42fddb5716e7849626
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-04-30 13:03:16 +0530

    Update 4bit binary counter.v

--- Circuits/Sequential circuit/Counters/4bit binary counter.v
+++ Circuits/Sequential circuit/Counters/4bit binary counter.v
@@ -11,3 +11,22 @@ module top_module (
     end
 
 endmodule
+
+
+
+
+//method1
+module top_module (
+    input clk,
+    input reset,      // Synchronous active-high reset
+    output [3:0] q);
+  
+    always@(posedge clk)begin
+        q<=4'b0000;
+        if(reset)
+          q<=4'b0000;
+        else
+          q<=q+4'b0001;
+    end
+
+endmodule
