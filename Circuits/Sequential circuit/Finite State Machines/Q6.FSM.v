3108ea74f4a449e48b20c135eaca24852d054b66
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-05-02 19:12:08 +0530

    Create Q6.FSM.v

---
+++ Circuits/Sequential circuit/Finite State Machines/Q6.FSM.v
@@ -0,0 +1,28 @@
+module top_module (
+    input clk,
+    input reset,     // synchronous reset
+    input w,
+    output z);
+    
+    parameter  A=3'b000,B=3'b001,C=3'b010,D=3'b011,E=3'b100,F=3'b101;
+    reg[2:0]state,next_state;
+    
+    always@(posedge clk)begin
+        if(reset)
+          state<=A;
+        else
+          state<=next_state;
+    end
+    always@(*)begin
+        case(state)
+            A:next_state=(w==0)?B:A;
+            B:next_state=(w==0)?C:D;
+            C:next_state=(w==0)?E:D;
+            D:next_state=(w==0)?F:A;
+            E:next_state=(w==0)?E:D;
+            F:next_state=(w==0)?C:D;
+            default:z=A;
+        endcase
+    end
+    assign z=(state==E | state==F);
+endmodule
