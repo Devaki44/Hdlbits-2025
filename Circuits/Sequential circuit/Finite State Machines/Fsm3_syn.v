19db217aa0a2d6d07fb0d218e303859da95df593
Devaki44 <146159374+Devaki44@users.noreply.github.com>
2025-05-02 08:38:36 +0530

    Create Fsm3_syn.v

---
+++ Circuits/Sequential circuit/Finite State Machines/Fsm3_syn.v
@@ -0,0 +1,33 @@
+module top_module(
+    input clk,
+    input in,
+    input reset,
+    output out);
+    
+    parameter A=2'b00;
+    parameter B=2'b01;
+    parameter C=2'b10;
+    parameter D=2'b11;
+    reg [1:0]state,next_state;
+    
+     // State flip-flops with asynchronous reset
+    always@(posedge clk)begin
+        if(reset)
+           state<=A;
+        else
+            state<=next_state;
+    end
+
+    // State transition logic
+    always@(*)begin
+        case(state)
+            A:next_state=(in==0)?A:B;
+            B:next_state=(in==0)?C:B;
+            C:next_state=(in==0)?A:D;
+            D:next_state=(in==0)?C:B;
+        endcase
+    end
+    // Output logic
+    assign out=(state==D);
+
+endmodule
